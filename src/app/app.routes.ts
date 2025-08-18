18b9a9d96879d1b1606f3ed3178cfa1bdc15edd8
Rafael Dias <rafaeldias@Mac.home>
2025-08-18 13:16:36 -0300

    feat: criacao do chatboot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent, title: 'Início' },
+    { path: ':section', component: HomeComponent, title: 'Início' },
+    { path: '**', redirectTo: '' },
+];
